647dc834b8d05d429bec8d299aed8db1c64c50f3
JansenFung <jansen.fung21@gmail.com>
2023-06-05 18:22:58 -0400

    use Drawer navigation

--- App.js
+++ App.js
@@ -2,15 +2,27 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import LogInScreen from './LogInScreen';
 import SignUpScreen from './SignUpScreen';
+import 'react-native-gesture-handler';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { NavigationContainer } from '@react-navigation/native';
+import AddPostScreen from './AddPostScreen';
 
+const Drawer = createDrawerNavigator();
 
 export default function App() {
   return (
+    <NavigationContainer>
+      <Drawer.Navigator>
+        <Drawer.Screen name="Posting" component={AddPostScreen}/>
+        <Drawer.Screen name="LogIn" component={LogInScreen}/>
+        <Drawer.Screen name="SignUp" component={SignUpScreen}/>
+      </Drawer.Navigator>
+    </NavigationContainer>
     // <View style={styles.container}>
     //   <Text>Open up App.js to start working on your app!</Text>
     //   <StatusBar style="auto" />
     // </View>
-    <SignUpScreen/>
+    //<AddPostScreen/>
   );
 }
 
