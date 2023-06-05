eb1cdd67e9fc5a2af8ae59b578beb661bf8e2e92
JansenFung <jansen.fung21@gmail.com>
2023-06-05 03:17:00 -0400

    update AddPostScreen UI

--- AddPostScreen.js
+++ AddPostScreen.js
@@ -1,12 +1,28 @@
-import { ScrollView,Text } from "react-native";
+import { View,Text, TextInput } from "react-native";
+import styleSheet from "./assets/StyleSheet";
+import { useState } from "react";
 
 const AddPostScreen = () =>{
+    const [title, setTitle] = useState('');
+    const [body, setBody] = useState('');
+
     return(
-        <ScrollView>
-            <Text>
-                Post
-            </Text>
-        </ScrollView>
+        <View style={styleSheet.postingContainer}>
+            <TextInput 
+            style={styleSheet.titleTextInputStyle} 
+            placeholder="Title" 
+            value={title} 
+            onChangeText={setTitle} 
+            multiline={true}
+            />
+            <TextInput 
+            style={styleSheet.bodyTextInputStyle} 
+            placeholder="Body" 
+            value={body} 
+            onChangeText={setBody} 
+            multiline={true} 
+            />
+        </View>
     );
 }
 
