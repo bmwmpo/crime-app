f33ea3b9978a69cf6a840920c09bd90114aca102
JansenFung <jansen.fung21@gmail.com>
2023-06-04 23:33:35 -0400

    create AddPostScreen

---
+++ AddPostScreen.js
@@ -0,0 +1,13 @@
+import { ScrollView,Text } from "react-native";
+
+const AddPostScreen = () =>{
+    return(
+        <ScrollView>
+            <Text>
+                Post
+            </Text>
+        </ScrollView>
+    );
+}
+
+export default AddPostScreen;
