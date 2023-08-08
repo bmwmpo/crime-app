3fb74b15e1c4ed308932abd3e1db186f1711730a
JansenFung <jansen.fung21@gmail.com>
2023-08-08 18:42:45 -0400

    create snapshot testing on AllCrimeStoriesScreen

---
+++ __test__/UI_Snapshot.test.js
@@ -0,0 +1,12 @@
+import { render, screen, fireEvent } from "@testing-library/react-native";
+import AllCrimeStoriesScreen from "../screen/CrimeStory/AllCrimeStoriesScreen";
+
+
+describe("rSnapshot testing", () => {
+  //test UI
+  it("allCrimeStories screen snapshot testing", () => {
+    const tree = render(<AllCrimeStoriesScreen />).toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+});
